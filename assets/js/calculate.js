d8f8630a8cfc4de9b762962ac4ea9c5c3e2d799c
linxiagong <linxiagong@gmail.com>
2017-01-30 16:59:09 +0100

    Update calculate.js

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -4,6 +4,15 @@ var _count_of_prime;
 var smallNum = 1;
 var largeNum  = 0;
 
+var isInt = function(x) {
+	if (isNaN(x)) {
+            return false;
+        }
+        if (x > -9007199254740992 && x <= 9007199254740992 && Math.floor(x) == x) {
+            return true;
+	}
+};
+
 function display_result(smallNum){
 	//alert(_number_of_user);
 	//alert('prime numbers = ' + _count_of_prime);
@@ -20,7 +29,10 @@ function prime_counting(){
 	_count_of_prime = 0;
 	_number_of_user = document.getElementById('_number_of_user').value;
 	//alert(_number_of_user);
-	
+	if (!isInt(_number_of_user)) {
+		document.getElementById('_count_of_prime').innerHTML = " Please enter integers :) ";
+		return;
+	}
 	if (_number_of_user <= 1000000000) {
 		_count_of_prime = Primesieve(_number_of_user, smallNum);
 		display_result(smallNum);
