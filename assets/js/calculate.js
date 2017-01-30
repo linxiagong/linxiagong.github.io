1fcea996c816a915db7dd0e911547e803f483b1d
GONGLinxia <linxia@eduroam.eurecom.fr>
2017-01-30 17:17:36 +0100

    modified:   assets/js/calculate.js

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -3,6 +3,7 @@ var _number_of_user;
 var _count_of_prime;
 var smallNum = 1;
 var largeNum  = 0;
+var E_NOT_INT = -1;
 
 var isInt = function(x) {
 	if (isNaN(x)) {
@@ -13,14 +14,16 @@ var isInt = function(x) {
 	}
 };
 
-function display_result(smallNum){
+function display_result(state){
 	//alert(_number_of_user);
 	//alert('prime numbers = ' + _count_of_prime);
 	document.getElementById('hidden').style.display = "inline-block";
-	if(smallNum) {
+	if(state == 1) {
 		document.getElementById('_count_of_prime').innerHTML = "numbers of primes smaller than "+_number_of_user+" = "+_count_of_prime;
-	} else {
+	} else if(state == 0) {
 		document.getElementById('_count_of_prime').innerHTML = "number over 1 billion: estimated count for "+_number_of_user+" = "+_count_of_prime;
+	} else if (state == -1) {
+		document.getElementById('_count_of_prime').innerHTML = " Please enter integers :) ";
 	}
 	
 }
@@ -30,11 +33,8 @@ function prime_counting(){
 	_number_of_user = document.getElementById('_number_of_user').value;
 	//alert(_number_of_user);
 	if (!isInt(_number_of_user)) {
-		alert(" Please enter integers :) ");
-		document.getElementById('_count_of_prime').innerHTML = " Please enter integers :) ";
-		return;
-	}
-	if (_number_of_user <= 1000000000) {
+		display_result(E_NOT_INT);
+	}else if (_number_of_user <= 1000000000) {
 		_count_of_prime = Primesieve(_number_of_user, smallNum);
 		display_result(smallNum);
 	} else {
