77db795733df69b9efebd8ae62569c673d47b363
linxiagong <linxiagong@users.noreply.github.com>
2023-08-02 06:14:25 +0000

    deploy: linxiagong/hugo-blogs@9961dbb90bcaf6b7eb451fa1f7c5213c3bfaf3fa

---
+++ docs/generative-models/diffusion/cite.js
@@ -0,0 +1,25 @@
+const bibliography = {
+    // "author_year_title": {
+    //     "author": "Author(s)",
+    //     "title": "Title of the Paper",
+    //     "journal": "Journal Name",
+    //     "year": "Year",
+    //     "volume": "Volume",
+    //     "number": "Issue",
+    //     "pages": "Page numbers",
+    //     "doi": "DOI (if available)",
+    //     "link": "",
+    // },
+    // Add more citations here
+    "kingma2013auto": {
+        "author": "Kingma, Diederik P and Welling, Max",
+        "title": "Auto-Encoding Variational Bayes",
+        "journal": "arXiv:1312.6114",
+        "year": "2013",
+        "volume": "Volume",
+        "number": "Issue",
+        "pages": "Page numbers",
+        "doi": "DOI (if available)",
+        "link": "https://arxiv.org/abs/1312.6114",
+    }
+};
